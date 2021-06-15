8a41e1e1a9504cf6c7763338434377b2802dd78f
Nick Carver <nicholascarver2@outlook.com>
2021-06-15 09:21:55 -0500

    added a modal, an input button, and a getwhat function

--- src/components/profile/ProfileProvider.js
+++ src/components/profile/ProfileProvider.js
@@ -4,17 +4,27 @@ export const ProfileContext = createContext()
 
 export const ProfileProvider = (props) => {
     const getProfile = () => {
-        return fetch("http://localhost:8000/users/my_profile",{
+        return fetch("http://localhost:8000/users/my_profile", {
             method: "GET",
-            headers:{
-                "Content-Type":"application/json",
+            headers: {
+                "Content-Type": "application/json",
                 "Authorization": `Token ${localStorage.getItem("priority_user_token")}`
             }
         })
-        .then(res => res.json())
-}
+            .then(res => res.json())
+    }
+    const getWhat = () => {
+        return fetch("http://localhost:8000/what", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("priority_user_token")}`
+            }
+        })
+            .then(res => res.json())
+    }
     return (
-        <ProfileContext.Provider value={{ getProfile }}>
+        <ProfileContext.Provider value={{ getProfile, getWhat }}>
             {props.children}
         </ProfileContext.Provider>
     )
