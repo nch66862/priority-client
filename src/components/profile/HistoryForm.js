524270e4a6e9d1975b52db7048ae37ebbd10a3aa
Nick Carver <nicholascarver2@outlook.com>
2021-06-15 14:07:05 -0500

    added a date picker package

--- src/components/profile/HistoryForm.js
+++ src/components/profile/HistoryForm.js
@@ -1,28 +1,43 @@
 import React, { useContext, useEffect, useState } from "react"
 import { ProfileContext } from "./ProfileProvider"
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Label } from 'reactstrap';
 import { RadioGroup, RadioButton } from 'react-radio-buttons';
+import DatePicker from 'react-date-picker';
 
 //just a container for a footer for completeness
 export const HistoryForm = ({ userProfile, toggle, modal }) => {
     const { getWhat, submitHistory } = useContext(ProfileContext)
     const [what, setWhat] = useState([])
     const [historyEvent, setHistoryEvent] = useState({
-        what: "",
-        time: userProfile.priority?.how
+        what_id: "",
+        time_spent: userProfile.priority?.how,
+        goal_date: new Date()
     })
+    // dates
+    // account creation day
+    const original_date = new Date(userProfile.priority.creation_date)
+    // current day
+    const today = new Date()
+    // see if history on those days and days in between
+    // ask user 1 by 1 starting with current day and working backwards
     useEffect(() => {
         getWhat()
             .then(response => setWhat(response))
     }, [])
     const handleChangeWhat = (whatId) => {
-        let newHistoryEvent = {...historyEvent}
-        newHistoryEvent.what = whatId
+        let newHistoryEvent = { ...historyEvent }
+        newHistoryEvent.what_id = whatId
         setHistoryEvent(newHistoryEvent)
     }
     const handleChangeTime = (event) => {
-        let newHistoryEvent = {...historyEvent}
-        newHistoryEvent.time = event.target.value
+        let newHistoryEvent = { ...historyEvent }
+        newHistoryEvent.time_spent = event.target.value
+        setHistoryEvent(newHistoryEvent)
+    }
+    const handleChangeDate = (date) => {
+        debugger
+        let newHistoryEvent = { ...historyEvent }
+        newHistoryEvent.goal_date = date
         setHistoryEvent(newHistoryEvent)
     }
     const handleSubmitHistory = () => {
@@ -39,8 +54,13 @@ export const HistoryForm = ({ userProfile, toggle, modal }) => {
                     })}
                 </RadioGroup>
                 <div>
-                    for <input onChange={handleChangeTime} name="time" type="number" max={480} min={5} step={5} value={historyEvent.time} required /> minutes today.
+                    for <input onChange={handleChangeTime} type="number" max={480} min={5} step={5} value={historyEvent.time} required /> minutes today.
                 </div>
+                <Label>Select A Day</Label>
+                <DatePicker
+                    onChange={handleChangeDate}
+                    value={historyEvent.goal_date}
+                />
             </ModalBody>
             <ModalFooter>
                 <Button color="primary" onClick={handleSubmitHistory}>Submit</Button>
