76ca7ec716282d7dd42e46ef7222206145d2b5e8
Nick Carver <nicholascarver2@outlook.com>
2021-06-18 16:50:20 -0500

    got the subscribe button to show up on the list of community profiles

--- src/components/users/Community.js
+++ src/components/users/Community.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react"
 import { useHistory } from "react-router-dom";
-import { ListGroup, ListGroupItem, Label, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+import { ListGroup, ListGroupItem, Label, ListGroupItemHeading, ListGroupItemText, Button } from 'reactstrap';
 import { UserContext } from "./UserProvider";
 import './Community.css'
 
@@ -13,7 +13,8 @@ export const Community = () => {
     }, [])
     const handleOpenProfile = (event) => {
         history.push(`/profiles/${event.target.id}`
-    )}
+        )
+    }
     return (
         <div>
             <h1>Community</h1>
@@ -22,9 +23,14 @@ export const Community = () => {
                     return (
                         <div className="communityProfileListItem" key={singleProfile.id} id={singleProfile.id} onClick={handleOpenProfile}>
                             <ListGroupItem>
-                                <Label className="communityProfileListItem" id={singleProfile.id}>{singleProfile.priority_user.user.first_name}</Label>
-                                <ListGroupItemHeading className="communityProfileListItem" id={singleProfile.id}>{singleProfile.priority}</ListGroupItemHeading>
-                                <ListGroupItemText className="communityProfileListItem" id={singleProfile.id}>{singleProfile.why}</ListGroupItemText>
+                                <div className="flexSubscribeButton">
+                                    <div>
+                                        <Label className="communityProfileListItem" id={singleProfile.id}>{singleProfile.priority_user.user.first_name}</Label>
+                                        <ListGroupItemHeading className="communityProfileListItem" id={singleProfile.id}>{singleProfile.priority}</ListGroupItemHeading>
+                                        <ListGroupItemText className="communityProfileListItem" id={singleProfile.id}>{singleProfile.why}</ListGroupItemText>
+                                    </div>
+                                    <Button disabled={singleProfile.priority_user.subscribed}>{singleProfile.priority_user.subscribed ? "Subscribed" : "+ Subscribe"}</Button>
+                                </div>
                             </ListGroupItem>
                         </div>
                     )
--- src/components/users/UserProvider.js
+++ src/components/users/UserProvider.js
@@ -6,56 +6,32 @@ export const UserProvider = (props) => {
     const [profiles, setProfiles] = useState([])
     const [publicProfile, setPublicProfile] = useState({})
     const logUserIn = (credentials) => {
-        return fetch("http://localhost:8000/login",{
+        return fetch("http://localhost:8000/login", {
             method: "POST",
-            headers:{
-                "Content-Type":"application/json",
+            headers: {
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(credentials)
         })
-        .then(res => res.json())
-}
-    const getPublicProfiles = () => {
-        return fetch("http://localhost:8000/users",{
-            headers:{
-                "Authorization": `Token ${localStorage.getItem("priority_user_token")}`
-            }
-        })
-        .then(res => res.json())
-        .then(setProfiles)
-    }
-    const getProfileById = (profileId) =>{
-        return fetch(`http://localhost:8000/users/${profileId}`,{
-            headers:{
-                "Authorization": `Token ${localStorage.getItem("priority_user_token")}`
-            }
-        })
-        .then(res => res.json())
-        .then(res => setPublicProfile(res))
-    }
-    const checkSubscribed = (authorId) => {
-        return fetch("http://localhost:8000/users/subscription_status", {
-            method: "POST",
-            headers:{
-                "Content-Type":"application/json",
-                "Authorization": `Token ${localStorage.getItem("priority_user_token")}`
-            },
-            body: JSON.stringify({
-                "author_id": authorId
-            })
-        })
             .then(res => res.json())
     }
-    const changeSubscribed = (subscribing, subscription) => {
-        return fetch("http://localhost:8000/users/subscription",{
-            method: subscribing ? "POST" : "DELETE",
-            // method:"POST",
-            headers:{
-                "Content-Type":"application/json",
+    const getPublicProfiles = () => {
+        return fetch("http://localhost:8000/users", {
+            headers: {
                 "Authorization": `Token ${localStorage.getItem("priority_user_token")}`
-            },
-            body: JSON.stringify(subscription)
+            }
         })
+            .then(res => res.json())
+            .then(setProfiles)
+    }
+    const getProfileById = (profileId) => {
+        return fetch(`http://localhost:8000/users/${profileId}`, {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("priority_user_token")}`
+            }
+        })
+            .then(res => res.json())
+            .then(res => setPublicProfile(res))
     }
     const checkAuthenticated = () => {
         return fetch(`http://localhost:8000/check-active`, {
@@ -71,11 +47,12 @@ export const UserProvider = (props) => {
     }
 
     return (
-        <UserContext.Provider value={{ 
-            getPublicProfiles, profiles, changeSubscribed, 
-            checkSubscribed, checkAuthenticated, 
+        <UserContext.Provider value={{
+            getPublicProfiles, profiles,
+            checkAuthenticated,
             logUserIn, getProfileById,
-            publicProfile }}>
+            publicProfile
+        }}>
             {props.children}
         </UserContext.Provider>
     )
