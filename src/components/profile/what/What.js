86984ab1051a214df9dfd46b79ee2e2184f3ac16
Nick Carver <nicholascarver2@outlook.com>
2021-06-18 15:12:27 -0500

    added a modal to stop a user from deleting all their whats

--- src/components/profile/what/What.js
+++ src/components/profile/what/What.js
@@ -1,12 +1,13 @@
 import React, { useContext, useEffect, useState } from "react"
 import { ProfileContext } from "../ProfileProvider"
-import { Button, ListGroup, ListGroupItem, Input } from 'reactstrap';
+import { Button, ListGroup, ListGroupItem, Input, Modal, ModalBody } from 'reactstrap';
 import { useParams } from "react-router-dom";
 
 export const What = ({ profile }) => {
     const { profileId } = useParams()
     const { getWhat, saveWhat, deleteWhat, whats } = useContext(ProfileContext)
     const [editMode, setEditMode] = useState(false)
+    const [deleteWhatError, setDeleteWhatError] = useState(false)
     const [what, setWhat] = useState({
         what: "",
         priority_id: profile.priority?.id
@@ -22,13 +23,18 @@ export const What = ({ profile }) => {
         // eslint-disable-next-line
     }, [profile])
     const toggleEditMode = () => setEditMode(!editMode)
+    const toggleDeleteWhatError = () => setDeleteWhatError(!deleteWhatError)
     const handleInputChange = (event) => {
         let newWhat = { ...what }
         newWhat.what = event.target.value
         setWhat(newWhat)
     }
     const handleDeleteWhat = (event) => {
-        deleteWhat(event.target.id)
+        if (whats.length > 1) {
+            deleteWhat(event.target.id)
+        } else {
+            toggleDeleteWhatError()
+        }
     }
     const handleSubmitWhat = () => {
         saveWhat(what)
@@ -39,6 +45,12 @@ export const What = ({ profile }) => {
     }
     return (
         <div>
+            <Modal isOpen={deleteWhatError} toggle={toggleDeleteWhatError}>
+                <ModalBody>
+                    <h4>You need at least one way to reach your goal</h4>
+                    <Button color="secondary" onClick={toggleDeleteWhatError}>OK</Button>
+                </ModalBody>
+            </Modal>
             <div className="whatHeader">
                 <h3>What will I prioritize?</h3>
                 {!profileId && <Button color="secondary" onClick={toggleEditMode}>{editMode ? "Done" : "Edit"}</Button>}
@@ -55,7 +67,7 @@ export const What = ({ profile }) => {
             </ListGroup>
             {editMode && (
                 <>
-                    <Input onChange={handleInputChange} value={what.what} id="what" type="text" name="what" />
+                    <Input onChange={handleInputChange} value={what.what} id="what" type="text" name="what" placeholder="type an activity"/>
                     <Button onClick={handleSubmitWhat}>Save</Button>
                 </>
             )}
